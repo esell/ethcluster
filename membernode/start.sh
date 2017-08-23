1c25622bcdd8975e410df266130bc2441cbc2e5d
esell <esell@esheavyindustries.com>
2017-08-23 08:37:36 -0600

    expose http-rpc and ws-rpc endpoints on member node

--- membernode/start.sh
+++ membernode/start.sh
@@ -6,7 +6,7 @@ if [ $? = 0 ]; then
     sleep 10
     BOOTNODE=`cat /root/bootstrap/boot_address`
     BOOTNODE_IP=`getent hosts bootnode | cut -d" " -f1`
-    geth --networkid=123454 --bootnodes="enode://$BOOTNODE@$BOOTNODE_IP:30301"
+    geth --networkid=123454 --bootnodes="enode://$BOOTNODE@$BOOTNODE_IP:30301" --rpc --rpcaddr "0.0.0.0" --ws --wsaddr "0.0.0.0"
 else
     echo "init of genesis block failed, exiting..."
     return 1
